d52018b468fe64bbbd26aa791310c64b1fff88b4
Animesh Das <audas007@gmail.com>
2016-08-07 16:51:56 -0700

    data science R program assignment - matrix inversion using cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## writing two functions to retrieve cached copy of the inverse of a matrix - if it exists in cache, 
+## otherwise we compute the inverse and add it to cache
 
-## Write a short comment describing this function
-
+## special matrix object that can cache
+## the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(z) i <<- z
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## method checks if inverse of matrix already exists
+## in the special matrix object; if not, it computes
+## the inverse using solve(), and updates the
+## special matrix object
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
